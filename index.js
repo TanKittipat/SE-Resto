f52155048ec218cb491303c2e8c5e318eae6d092
TanKittipat <kittipat12tan@gmail.com>
2023-02-16 16:28:56 +0700

    2023/02/16

--- index.js
+++ index.js
@@ -20,3 +20,18 @@ close_icon.onclick = () => {
   let search_form = document.querySelector("#search-form");
   search_form.classList.remove("active");
 };
+
+// Swiper Function
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000, //time
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
