b42bae7451c34c4ef7b5a972c6da28f22845fdd6
TanKittipat <kittipat12tan@gmail.com>
2023-03-09 10:34:28 +0700

    23/03/09

--- index.js
+++ index.js
@@ -63,3 +63,13 @@ var swiper = new Swiper(".review-slider", {
     },
   },
 });
+
+// Loader Section
+
+function loader() {
+  document.querySelector(".loader-container").classList.add("fade-out");
+}
+function fadeOut() {
+  setInterval(loader, 2500);
+}
+window.onload = fadeOut;
